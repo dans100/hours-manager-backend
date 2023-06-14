f51107bef57e2f3b8df7852adc34661bc318f6aa
Daniel Sulkowski <dansulk@gmail.com>
2023-06-14 17:27:25 +0200

    added cookieParser, and GlobalPipes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { corsConfig } from './config/cors.config';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { pipesConfig } from './utils/pipes-config';
 
 async function bootstrap() {
   const { PORT } = process.env;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe(pipesConfig));
   app.enableCors(corsConfig);
+  app.use(cookieParser());
   await app.listen(PORT);
 }
 bootstrap();
