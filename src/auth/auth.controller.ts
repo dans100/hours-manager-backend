67864b1591cb62072e3817e822e9ac66a4153a3e
Daniel Sulkowski <dansulk@gmail.com>
2023-06-14 17:16:21 +0200

    added path and method to logout user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './local-auth.guard';
+import { Body, Controller, Delete, Post, Res, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthLoginDto } from './dto/auth-login.dto';
+import { Response } from 'express';
+import { UserObj } from '../decorators/user-obj.decorator';
+import { User } from '../users/entities/user.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-  @UseGuards(LocalAuthGuard)
+  constructor(private authService: AuthService) {}
+
   @Post('login')
-  async login(@Request() req) {
-    return req.user;
+  async login(@Body() req: AuthLoginDto, @Res() res: Response) {
+    return this.authService.login(req, res);
+  }
+
+  @Delete('logout')
+  @UseGuards(AuthGuard('jwt'))
+  async logout(@UserObj() user: User, @Res() res: Response) {
+    return this.authService.logout(user, res);
   }
 }
