42e71c1b5f2de4061cd4247c8b56c63e2ac17292
Daniel Sulkowski <dansulk@gmail.com>
2023-06-14 17:20:03 +0200

    added to exports AuthService and JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
-import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [
-    UsersModule,
-    PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_ACCESS_KEY,
-      signOptions: { expiresIn: '15m' },
-    }),
-  ],
-  providers: [AuthService, LocalStrategy],
+  imports: [UsersModule],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
