52f5efd9ba3b638dd0cd45a2ac1aa41e5534dd99
Daniel Sulkowski <dansulk@gmail.com>
2023-06-14 17:32:34 +0200

    added method to create new user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { hashPwd } from '../utils/hash-pwd';
 
 @Injectable()
 export class UsersService {
   async findOne(username: string): Promise<User | undefined> {
     return User.findOneBy({ username });
+  }
+
+  async create(user: CreateUserDto) {
+    const newUser = new User();
+    newUser.username = user.username;
+    newUser.role = user.role;
+    newUser.hashedPassword = await hashPwd(user.password);
+    await newUser.save();
   }
 }
