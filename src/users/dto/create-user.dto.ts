adf2794816bca690b214f947f6dec8f6f180375b
Daniel Sulkowski <dansulk@gmail.com>
2023-06-15 21:11:36 +0200

    added CreateUserDto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+
+  @IsString()
+  role: string;
+}
