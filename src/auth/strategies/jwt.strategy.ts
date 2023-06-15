f994e88dbae966fff19705c92d1d9c5adbc0f186
Daniel Sulkowski <dansulk@gmail.com>
2023-06-15 21:17:50 +0200

    added JwtStrategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,46 @@
+import { Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+import { ConfigService } from '@nestjs/config';
+import { User } from '../../users/entities/user.entity';
+
+export interface JwtPayload {
+  id: string;
+}
+
+function cookieExtractor(req: any): null | string {
+  return req && req.cookies ? req.cookies?.jwt ?? null : null;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @Inject(forwardRef(() => ConfigService))
+    private configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: cookieExtractor,
+      secretOrKey: configService.get('JWT_ACCESS_KEY'),
+    });
+  }
+
+  async validate(payload: JwtPayload, done: (error, user) => void) {
+    if (!payload || !payload.id) {
+      return done(new UnauthorizedException(), false);
+    }
+
+    const user = await User.findOneBy({ currentToken: payload.id });
+
+    if (!user) {
+      return done(new UnauthorizedException(), false);
+    }
+
+    done(null, user);
+  }
+}
